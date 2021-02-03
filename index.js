bc47ed18ca2a484d28e76b70856b60ca3df75012
Thomas Hammond <thomas_hammond@berkeley.edu>
2021-02-03 14:37:46 -0800

    Refactoring index.js and adding twitter share

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // Variables
-var simonPattern = [];
+var gamePattern = [];
 var playerPattern = [];
-var level = 0;
+var level = 1;
 var index = 0;
 var padColors = ["red", "blue", "green", "yellow"];
 var gameModes = ["classic", "single", "graymode", "reverse", "creator"];
 var currentMode = "classic";
-var sounds = {
-  red: new Audio("simon_sounds/red.mp3"),
-  blue: new Audio("simon_sounds/blue.mp3"),
-  green: new Audio("simon_sounds/green.mp3"),
-  yellow: new Audio("simon_sounds/yellow.mp3"),
-};
+var playPattern = defaultPattern;
+var intervalHandle = null;
 
 const triviaText = [
   "SIMON is an electronic game of memory skill invented by Ralph H. Baer and Howard J. Morrison and released by the American board game company Milton Bradley in 1978.",
@@ -21,85 +17,48 @@ const triviaText = [
   "SIMON's tones were designed to always be harmonic, no matter the sequence, and consisted of an A major triad in second inversion, resembling a trumpet fanfare",
 ];
 
-// logic functions
-function updateSimonPattern() {
+// Adds a new random color to the gamePattern
+function updatePattern() {
   let randomNum = Math.floor(Math.random() * 4);
-  let randomColor = this.padColors[randomNum];
-  this.currentMode !== "reverse"
-    ? this.simonPattern.push(randomColor)
-    : this.simonPattern.unshift(randomColor);
-  playPattern(randomColor);
+  let randomColor = padColors[randomNum];
+  currentMode !== "reverse"
+    ? gamePattern.push(randomColor)
+    : gamePattern.unshift(randomColor);
   return randomNum;
 }
 
 function checkAnswer(currentIndex) {
-  // Check for correct sequence element
-  if (this.playerPattern[currentIndex] != this.simonPattern[currentIndex]) {
+  if (playerPattern[currentIndex] != gamePattern[currentIndex]) {
     let audio = new Audio("simon_sounds/wrong.mp3");
     audio.play();
     gameOver();
-    resetGame();
     return;
   }
-  // Check if pattern is complete
-  if (currentIndex === this.level) {
-    this.playerPattern = [];
-    this.index = 0;
+  // Check if current sequence is complete
+  if (currentIndex === level - 1) {
+    playerPattern = [];
+    index = 0;
     level += 1;
-    updateScore();
+    updateRound();
     setTimeout(() => {
-      updateSimonPattern();
+      updatePattern();
+      playPattern();
     }, 500);
   } else {
-    this.index += 1;
-  }
-}
-
-function playPattern(randomColor) {
-  switch (this.currentMode) {
-    case "single":
-      setTimeout(() => {
-        playSound(randomColor);
-        animatePad(randomColor);
-      }, 500);
-      break;
-    case "reverse":
-      var i = this.simonPattern.length - 1;
-      setInterval(function () {
-        if (i >= 0) {
-          playSound(this.simonPattern[i]);
-          animatePad(this.simonPattern[i]);
-          i--;
-        } else {
-          return;
-        }
-      }, 500);
-      break;
-    default:
-      var i = 0;
-      var len = this.simonPattern.length;
-      setInterval(function () {
-        if (i < len) {
-          playSound(this.simonPattern[i]);
-          animatePad(this.simonPattern[i]);
-          i++;
-        } else {
-          return;
-        }
-      }, 500);
-      break;
+    index += 1;
   }
 }
 
 function setGameMode(mode) {
+  // Do nothing if mode is currentMode
   if (!mode) {
     return;
   }
   resetGame();
   document.getElementById("mode-title").textContent = mode.toUpperCase();
-  document.querySelector("#score").textContent = "THINK FAST!";
-  this.currentMode = mode;
-  this.gameModes.forEach((gamemode) => {
+  document.querySelector("#round").textContent = "THINK FAST!";
+  currentMode = mode;
+  gameModes.forEach((gamemode) => {
     if (gamemode !== mode) {
       document
         .getElementById(gamemode)
@@ -116,19 +75,106 @@ function setGameMode(mode) {
         .children[0].classList.add("gamemode-active");
     }
   });
-  setGrayMode(this.currentMode === "graymode");
+  // Deal with graymode
+  currentMode === "graymode"
+    ? padColors.forEach((color) => {
+        document.getElementById(color).classList.add("gray");
+      })
+    : padColors.forEach((color) => {
+        document.getElementById(color).classList.remove("gray");
+      });
+  // Assign playPattern to currentMode
+  if (currentMode === "single") {
+    playPattern = singlePattern;
+  } else if (currentMode === "reverse") {
+    playPattern = reversePattern;
+  } else {
+    playPattern = defaultPattern;
+  }
 }
 
 function resetGame() {
-  this.simonPattern = [];
-  this.playerPattern = [];
-  this.level = 0;
-  this.index = 0;
+  clearInterval(intervalHandle);
+  gamePattern = [];
+  playerPattern = [];
+  level = 1;
+  index = 0;
+}
+
+function gameOver() {
+  let gameBoard = document.querySelector(".outer");
+  let triviaMenu = document.querySelector(".trivia");
+  let twitter = document.querySelector(".twitter-wrapper");
+  let finalRound = document.querySelector("#final-round");
+  finalRound.textContent = "Total Rounds: " + (level - 1);
+  gameBoard.classList.add("invisible");
+  let randomNum = Math.floor(Math.random() * triviaText.length);
+  document.getElementById("trivia-text").textContent = triviaText[randomNum];
+  triviaMenu.classList.remove("invisible");
+  twitter.classList.remove("invisible");
+  document.querySelector("#round").textContent = "GAME OVER";
+  document.querySelector(".play-again").addEventListener(
+    "click",
+    () => {
+      gameBoard.classList.remove("invisible");
+      triviaMenu.classList.add("invisible");
+      twitter.classList.add("invisible");
+
+      updatePattern();
+      playPattern();
+      updateRound();
+    },
+    { once: true }
+  );
+  resetGame();
+}
+
+function clickHandler(color) {
+  playSound(color);
+  animatePad(color);
+  playerPattern.push(color);
+  checkAnswer(index);
+}
+
+// Functions to assign to playPattern depending on gamemode
+function singlePattern() {
+  setTimeout(() => {
+    playSound(gamePattern[gamePattern.length - 1]);
+    animatePad(gamePattern[gamePattern.length - 1]);
+  }, 500);
+}
+
+function reversePattern() {
+  var i = gamePattern.length - 1;
+  intervalHandle = setInterval(function () {
+    if (i >= 0) {
+      playSound(gamePattern[i]);
+      animatePad(gamePattern[i]);
+      i--;
+    } else {
+      return;
+    }
+  }, 500);
+}
+
+function defaultPattern() {
+  var i = 0;
+  var len = gamePattern.length;
+  intervalHandle = setInterval(() => {
+    if (i < len) {
+      playSound(gamePattern[i]);
+      animatePad(gamePattern[i]);
+      i++;
+    } else {
+      return;
+    }
+  }, 500);
 }
 
 // UI handlers
 function playSound(color) {
-  this.sounds[color].play();
+  let audio = new Audio("simon_sounds/" + color + ".mp3");
+  audio.play();
 }
 
 function animatePad(color) {
@@ -138,60 +184,17 @@ function animatePad(color) {
   setTimeout(() => {
     pad.classList.add("show");
     pad.classList.remove("hide");
-  }, 100);
+  }, 50);
 }
 
-function updateScore() {
-  document.getElementById("score").textContent = "SCORE: " + this.level;
-}
-
-function clickHandler(color) {
-  playSound(color);
-  animatePad(color);
-  this.playerPattern.push(color);
-  checkAnswer(this.index);
-}
-
-function gameOver() {
-  let gameBoard = document.querySelector(".outer");
-  let triviaMenu = document.querySelector(".trivia");
-  let finalScore = document.querySelector("#final-score");
-  finalScore.textContent = "SCORE: " + this.level;
-  gameBoard.classList.add("invisible");
-  setTriviaText();
-  triviaMenu.classList.remove("invisible");
-  document.querySelector("#score").textContent = "GAME OVER";
-  document.querySelector(".play-again").addEventListener(
-    "click",
-    () => {
-      gameBoard.classList.remove("invisible");
-      triviaMenu.classList.add("invisible");
-      updateSimonPattern();
-      updateScore();
-    },
-    { once: true }
-  );
-}
-
-function setTriviaText() {
-  let randomNum = Math.floor(Math.random() * triviaText.length);
-  document.getElementById("trivia-text").textContent = triviaText[randomNum];
-}
-
-function setGrayMode(graymode) {
-  graymode
-    ? this.padColors.forEach((color) => {
-        document.getElementById(color).classList.add("gray");
-      })
-    : this.padColors.forEach((color) => {
-        document.getElementById(color).classList.remove("gray");
-      });
+function updateRound() {
+  document.getElementById("round").textContent = "ROUND: " + level;
 }
 
 /*** Initialization ***/
 
 // Set up game pads
-this.padColors.forEach((pad) => {
+padColors.forEach((pad) => {
   document.getElementById(pad).addEventListener("touchstart", (event) => {
     clickHandler(event.target.id);
     event.stopPropagation();
@@ -207,8 +210,9 @@ this.padColors.forEach((pad) => {
 // Set up power button
 document.querySelector(".power-btn").addEventListener("click", () => {
   resetGame();
-  updateSimonPattern();
-  updateScore();
+  updateRound();
+  updatePattern();
+  playPattern();
 });
 
 // Set up gamemode buttons
